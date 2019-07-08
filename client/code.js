d07a04cfd25c19aaa484312f5a8fe05dd143af1b
Brennyn Simpson <bfsimpson5@gmail.com>
2019-07-08 11:30:18 -0600

    Columns are set up as individual objects and titles are displayed with delete icon more icon and the add button

--- client/code.js
+++ client/code.js
@@ -3,7 +3,33 @@ var app = new Vue ({
     el: "#app",
 
     data: {
-        columns: 12,
+        columns: [
+            {
+                title: "WISHLIST",
+                jobs: 6,
+            },
+            {
+                title: "APPLIED",
+                jobs: 5,
+            },
+            {
+                title: "PHONE",
+                jobs: 3,
+            },
+            {
+                title: "ON SITE",
+                jobs: 2,
+            },
+            {
+                title: "OFFER",
+                jobs: 1,
+            },
+            {
+                title: "REJECTED",
+                jobs: 0,
+            },
+        ]
+
 
     },
 
