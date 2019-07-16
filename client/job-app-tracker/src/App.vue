1ade4f1e09dfb8583198b092e7a72446c9635b09
Brennyn Simpson <bfsimpson5@gmail.com>
2019-07-16 09:46:02 -0600

    Added the Vue Cli and strared on components

---
+++ client/job-app-tracker/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+  <v-content>
+      <SideNav />
+      <TopNav />
+      <TopNavTwo />
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import SideNav from './components/SideNav.vue'
+import TopNav from './components/TopNav.vue'
+import TopNavTwo from './components/TopNavTwo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    SideNav,
+    TopNav,
+    TopNavTwo
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+
