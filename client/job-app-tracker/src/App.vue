3c90cfe1625b8aa69d1dd94720b89d3499a2e0bb
Dami <obayanjud@gmail.com>
2019-07-16 11:30:55 -0600

    lint code

--- client/job-app-tracker/src/App.vue
+++ client/job-app-tracker/src/App.vue
@@ -1,31 +1,28 @@
 <template>
   <v-app>
-  <v-content>
+    <v-content>
       <SideNav />
       <TopNav />
       <TopNavTwo />
-      <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import SideNav from './components/SideNav.vue'
-import TopNav from './components/TopNav.vue'
-import TopNavTwo from './components/TopNavTwo.vue'
+import SideNav from "./components/SideNav.vue";
+import TopNav from "./components/TopNav.vue";
+import TopNavTwo from "./components/TopNavTwo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     SideNav,
     TopNav,
     TopNavTwo
   },
-  data () {
-    return {
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
-
-
--- client/job-app-tracker/src/components/SideNav.vue
+++ client/job-app-tracker/src/components/SideNav.vue
@@ -1,30 +1,59 @@
 <template>
-    <v-navigation-drawer style="background-color: rgb(24, 0, 69)" dark permanent app width="70px" xs3>
-      <v-list>
-          <v-list-tile>
-              <v-list-tile-action>
-                <img :src="require('../assets/huntr_logo_mini.png')" height="auto" width="15px" class="mt-0 mr-4 ml-0"/>
-                <img :src="require('../assets/microphone.png')" height="auto" width="15px" class="mt-4 mr-4"/>
-                <img :src="require('../assets/thumbtack.png')" height="auto" width="15px" class="mt-4 mr-4"/>
-                <img :src="require('../assets/settings.png')" height="auto" width="15px" class="mt-4 mr-4"/>
-                <img :src="require('../assets/power-off.png')" height="auto" width="15px" class="mt-4 mr-4"/>
-
-              </v-list-tile-action>
-          </v-list-tile>
-      </v-list>
+  <v-navigation-drawer
+    style="background-color: rgb(24, 0, 69)"
+    dark
+    permanent
+    app
+    width="70px"
+    xs3
+  >
+    <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <img
+            :src="require('../assets/huntr_logo_mini.png')"
+            height="auto"
+            width="15px"
+            class="mt-0 mr-4 ml-0"
+          />
+          <img
+            :src="require('../assets/microphone.png')"
+            height="auto"
+            width="15px"
+            class="mt-4 mr-4"
+          />
+          <img
+            :src="require('../assets/thumbtack.png')"
+            height="auto"
+            width="15px"
+            class="mt-4 mr-4"
+          />
+          <img
+            :src="require('../assets/settings.png')"
+            height="auto"
+            width="15px"
+            class="mt-4 mr-4"
+          />
+          <img
+            :src="require('../assets/power-off.png')"
+            height="auto"
+            width="15px"
+            class="mt-4 mr-4"
+          />
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-        
-    }
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <style scoped>
-    background: blue;
+background: blue;
 </style>
