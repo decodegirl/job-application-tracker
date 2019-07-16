559a80d91bb647fdb0753f8991c2cb95e37da057
Dami <obayanjud@gmail.com>
2019-07-16 11:33:27 -0600

    no longer using .html files to route to pages

--- client/job-app-tracker/src/views/Home.vue
+++ client/job-app-tracker/src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+// import HelloWorld from "../components/HelloWorld";
 
 export default {
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
