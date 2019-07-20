f7bd9e9697b060bbf7fb68113b53d0e051e8545c
Dami <obayanjud@gmail.com>
2019-07-20 15:50:52 -0600

    fix card spacing

--- client/job-app-tracker/src/components/JobCard.vue
+++ client/job-app-tracker/src/components/JobCard.vue
@@ -1,52 +1,55 @@
 <template>
-  <v-dialog v-model="dialog" max-width="900">
-    <template v-slot:activator="{ on }">
-      <v-hover>
-        <v-card
-          v-on="on"
-          slot-scope="{ hover }"
-          xs12
-          dark
-          :color="job.color"
-          :max-width="width"
-          class="mb-2"
-        >
-          <v-list-tile :class="job.color" class="pt-2">
-            <v-list-tile-avatar :size="24" class="pb-2">
-              <v-img
-                :src="`//logo.clearbit.com/${job.image}.com`"
-                alt="avatar"
-              ></v-img>
-            </v-list-tile-avatar>
+  <div>
+    <v-dialog v-model="dialog" max-width="900">
+      <template v-slot:activator="{ on }">
+        <v-hover>
+          <v-card
+            v-on="on"
+            slot-scope="{ hover }"
+            xs12
+            dark
+            :color="job.color"
+            :max-width="width"
+            height="70"
+            class="mb-2 pb-4"
+          >
+            <v-list-tile :class="job.color" class="pt-2">
+              <v-list-tile-avatar :size="24" class="pb-2">
+                <v-img
+                  :src="`//logo.clearbit.com/${job.image}.com`"
+                  alt="avatar"
+                ></v-img>
+              </v-list-tile-avatar>
 
-            <v-list-tile-content>
-              <v-list-tile-title class="font-weight-regular">
-                {{ job.title }}
-              </v-list-tile-title>
-              <v-list-tile-sub-title class="font-weight-light">
-                {{ job.subTitle }}
+              <v-list-tile-content>
+                <v-list-tile-title class="font-weight-regular">
+                  {{ job.title }}
+                </v-list-tile-title>
+                <v-list-tile-sub-title class="font-weight-light">
+                  {{ job.subTitle }}
+                </v-list-tile-sub-title>
+              </v-list-tile-content>
+
+              <v-list-tile-action v-if="hover">
+                <v-btn icon ripple>
+                  <v-icon color="grey lighten-1"> delete_outline </v-icon>
+                </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-list-tile-content
+              class="text-xs-right pr-3 pt-4 pb-1"
+              :class="job.color"
+            >
+              <v-list-tile-sub-title class="font-weight-light caption">
+                added {{ job.date_added }} ago
               </v-list-tile-sub-title>
             </v-list-tile-content>
-
-            <v-list-tile-action v-if="hover">
-              <v-btn icon ripple>
-                <v-icon color="grey lighten-1"> delete_outline </v-icon>
-              </v-btn>
-            </v-list-tile-action>
-          </v-list-tile>
-          <v-list-tile-content
-            class="text-xs-right pr-3 pt-4 pb-1"
-            :class="job.color"
-          >
-            <v-list-tile-sub-title class="font-weight-light caption">
-              added {{ job.date_added }} ago
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-card>
-      </v-hover>
-    </template>
-    <JobDialog :job="job" @clickedSth="dialog = false" />
-  </v-dialog>
+          </v-card>
+        </v-hover>
+      </template>
+      <JobDialog :job="job" @clickedSth="dialog = false" />
+    </v-dialog>
+  </div>
 </template>
 
 <script>
