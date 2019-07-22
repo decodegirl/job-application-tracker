a41c5b0130c069dfdf95823e45d71c2c0d47ce8b
Brennyn Simpson <bfsimpson5@gmail.com>
2019-07-22 11:26:34 -0600

    The get, post, and delete are created on in the server.js file and they all work on postman

--- server/schemas/job.js
+++ server/schemas/job.js
@@ -1,35 +1,203 @@
 const mongoose = require("mongoose");
 
 const jobsSchema = mongoose.Schema({
-  company: {
+  id: {
+    type: Number,
+    required: false,
+    default: ""
+  },
+  column: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  color: {
     type: String,
-    required: true
+    required: true,
+    default: "blue"
   },
   title: {
     type: String,
-    required: true
+    required: true,
+    default: ""
   },
-  columnId: {
+  sub_title: {
     type: String,
-    required: true
+    required: true,
+    default: ""
   },
-  location: {
-    type: String,
-    required: false
-  },
-  salary: {
-    type: String,
-    required: false
-  },
-  postURL: {
-    type: String,
-    required: false
-  },
-  date: {
+  date_added: {
     type: String,
     required: true,
     default: new Date().toDateString()
-  }
+  },
+  image: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  location: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  salary: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  post_url: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  deadline: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  applied: {
+    type: Number,
+    required: false,
+    default: 0
+  },
+  interview1: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  interview2: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  offer: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  description: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  notes: {
+    type: String,
+    required: false,
+    default: ""
+  },
+  company_info: {
+    title: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    description: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    website: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    founded: {
+      type: Number,
+      required: false,
+      default: ""
+    },
+    type: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    country: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    industry: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    alexa_global: {
+      type: String,
+      required: false,
+      default: ""
+    },
+    alexa_usa: {
+      type: String,
+      required: false,
+      default: ""
+    }
+  },
+  todos: [
+    {
+      id: {
+        type: Number,
+        required: false
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      position: {
+        type: String,
+        required: false,
+        default: "+ add position"
+      },
+      image: {
+        type: String,
+        required: false
+      },
+      active: {
+        type: Boolean,
+        required: false,
+        default: true
+      }
+    }
+  ]
+  
+  
+  
+  
+  
+  
+  // id: {
+  //   type: Number,
+  //   required: true
+  // },
+  // company: {
+  //   type: String,
+  //   required: true
+  // },
+  // title: {
+  //   type: String,
+  //   required: true
+  // },
+  // columnId: {
+  //   type: String,
+  //   required: true
+  // },
+  // location: {
+  //   type: String,
+  //   required: false
+  // },
+  // salary: {
+  //   type: String,
+  //   required: false
+  // },
+  // postURL: {
+  //   type: String,
+  //   required: false
+  // },
+  // date: {
+  //   type: String,
+  //   required: true,
+  //   default: new Date().toDateString()
+  // }
 });
 
 var model = mongoose.model("jobs", jobsSchema);
--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ server.get("/stages", function(req, res) {
     });
 });
 
+// Get jobs
 server.get("/jobs", function(req, res) {
   jobsModel
     .find()
@@ -39,6 +40,80 @@ server.get("/jobs", function(req, res) {
     });
 });
 
+// Delete jobs
+server.delete("/jobs/:id", function( req, res ) {
+  jobsModel.findByIdAndDelete( req.params.id ).then( function( ){
+      res.status( 204 );
+      res.send( );
+  }).catch( function ( error ) {
+      var response = {
+          msg: error.message
+      };
+      res.status( 400 );
+      res.json( response );
+  });
+});
+
+// Delete stages
+server.delete("/stages/:id", function( req, res ) {
+  stagesModel.findByIdAndDelete( req.params.id ).then( function( ){
+      res.status( 204 );
+      res.send( );
+  }).catch( function ( error ) {
+      var response = {
+          msg: error.message
+      };
+      res.status( 400 );
+      res.json( response );
+  });
+});
+
+// Post Jobs
+server.post("/jobs", function(req, res) {
+  jobsModel
+    .create({
+      id: req.body.id,
+      column: req.body.column,
+      color: req.body.color,
+      title: req.body.title,
+      sub_title: req.body.sub_title,
+      date_added: req.body.date_added,
+      image: req.body.image,
+      location: req.body.location,
+      salary: req.body.salary,
+      post_url: req.body.post_url,
+      dealine: req.body.deadline,
+      applied: req.body.applied,
+      interview1: req.body.interview1,
+      interview2: req.body.interview2,
+      offer: req.body.offer,
+      description: req.body.description,
+      notes: req.body.notes,
+      company_info: {
+        title: req.body.company_info.title,
+        description: req.body.company_info.description,
+        website: req.body.company_info.website,
+        founded: req.body.company_info.founded,
+        type: req.body.company_info.type,
+        country: req.body.company_info.country,
+        industry: req.body.company_info.industry,
+        alexa_global: req.body.company_info.alexa_global,
+        alexa_usa: req.body.company_info.alexa_usa
+      },
+      todos: []
+    })
+    .then(function(job) {
+      res.status(201);
+      // log the job for development purposes
+      res.json(job);
+    })
+    .catch(function(err) {
+      res.status(400);
+      res.json({ msg: err.message });
+    });
+});
+
+// Post stages
 server.post("/stage", function(req, res) {
   stagesModel
     .create({
@@ -55,23 +130,6 @@ server.post("/stage", function(req, res) {
     });
 });
 
-server.post("/jobs", function(req, res) {
-  jobsModel
-    .create({
-      company: req.body.company,
-      title: req.body.title,
-      columnId: req.body.columnId
-    })
-    .then(function(job) {
-      res.status(201);
-      // log the job for development purposes
-      res.json(job);
-    })
-    .catch(function(err) {
-      res.status(400);
-      res.json({ msg: err.message });
-    });
-});
 
 // Start the server and connect to the database
 mongoose
