b17e4a6770d946ab8fee0729229d4f096a40d626
Celine <celinemwango2@gmail.com>
2019-07-26 08:49:01 -0600

    made changes to the side nav

--- job-app-tracker/src/components/SideNav.vue
+++ job-app-tracker/src/components/SideNav.vue
@@ -22,24 +22,32 @@
             width="15px"
             class="mt-4 mr-4"
           /> </a>
-          <a href = '#'> <img
+          <a class="tooltip" href = '#'> <img
             :src="require('../assets/thumbtack.png')"
             height="auto"
             width="15px"
             class="mt-4 mr-4"
-          /> </a>
-          <a href = '#'><img
+          /> 
+           <span class="tooltiptext">track</span>
+           </a>
+          <a class="tooltip" href = '#'><img
             :src="require('../assets/settings.png')"
             height="auto"
             width="15px"
             class="mt-4 mr-4"
-          /> </a>
-           <a href="/users/logout"><img
+          /> 
+           <span class="tooltiptext">settings</span>
+          </a>
+      
+           <a class="tooltip" href="/users/logout"><img
             :src="require('../assets/power-off.png')"
             height="auto"
             width="15px"
             class="mt-4 mr-4"
-          /></a>
+          />
+            <span class="tooltiptext">logout</span>
+          </a>
+         
         </v-list-tile-action>
       </v-list-tile>
     </v-list>
@@ -53,3 +61,32 @@ export default {
   }
 };
 </script>
+
+<style>
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
+}
+
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 80px;
+  background-color: #ccc;
+  color: #000;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+  position: absolute;
+  /* Position the tooltip */
+  z-index: 1;
+  top: -5px;
+  left: 105%;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+</style>
